f5aea3b385ec2a427200e1c0313016dc73f54739
luiscla1987 <luiscla1987@gmail.com>
2021-01-16 10:38:19 -0300

    Migrations

--- database/migrations/2021_01_16_130948_create_table_real_state_photos.php
+++ database/migrations/2021_01_16_130948_create_table_real_state_photos.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTableRealStatePhotos extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,16 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('real_state_photos', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('real_state_id');
+
+            $table->string('photo');
+            $table->boolean('is_thumb');
+
             $table->timestamps();
+
+            $table->foreign('real_state_id')->references('id')->on('real_state');
         });
     }
 
@@ -31,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('real_state_photos');
     }
 }
